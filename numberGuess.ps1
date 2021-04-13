99c2932e1cd726196477cbc388620a6183deffb2
BeardShell <78932365+BeardShell@users.noreply.github.com>
2021-04-13 14:12:07 +0200

    Create numberGuess.ps1

---
+++ numberGuess.ps1
@@ -0,0 +1,46 @@
+$maxValue = 10
+$answer = 0
+
+function playGame {
+    $answer = generateNumber -maxValue 10
+    userGuess
+}
+
+function generateNumber {
+    return Get-Random -Maximum $maxValue -Minimum 1
+}
+
+function userGuess {
+    $userInput = Read-Host "Raad het getal tussen de 1 en de $($maxValue): "
+    evaluateInput $userInput
+}
+
+function evaluateInput {
+    try {
+        $userInput = [int]$userInput
+    } catch [System.Management.Automation.RuntimeException] {
+        Write-Output "Voer aub. alleen een cijfer in!"
+        userGuess
+    }
+
+    if ($userInput -gt 0 -and $userInput -le $maxValue) {
+        if ($userInput -lt $answer) {
+            Write-Output "Het getal wat je zoekt is groter!"
+            userGuess
+        }
+        if ($userInput -gt $answer) {
+            Write-Output "Het getal wat je zoekt is kleiner!"
+            userGuess
+        }
+        if ($userInput -eq $answer) {
+            Write-Output "*** GEFELICITEERD, $($userInput) IS HET JUISTE ANTWOORD! ***"
+            break
+        }
+    } else {
+        Write-Output "Alleen getallen tussen de 1 en de $($maxValue)!"
+        userGuess
+    }
+}
+    
+
+playGame
