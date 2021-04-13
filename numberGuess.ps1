c35301e60aecd70c39314702931979b76fc413c4
BeardShell <78932365+BeardShell@users.noreply.github.com>
2021-04-13 17:44:48 +0200

    Update numberGuess.ps1

--- numberGuess.ps1
+++ numberGuess.ps1
@@ -2,7 +2,7 @@ $maxValue = 10
 $answer = 0
 
 function playGame {
-    $answer = generateNumber -maxValue 10
+    $answer = generateNumber -maxValue $maxValue
     userGuess
 }
 
@@ -24,20 +24,25 @@ function evaluateInput {
     }
 
     if ($userInput -gt 0 -and $userInput -le $maxValue) {
-        if ($userInput -lt $answer) {
-            Write-Output "Het getal wat je zoekt is groter!"
-            userGuess
-        }
-        if ($userInput -gt $answer) {
-            Write-Output "Het getal wat je zoekt is kleiner!"
-            userGuess
-        }
         if ($userInput -eq $answer) {
             Write-Output "*** GEFELICITEERD, $($userInput) IS HET JUISTE ANTWOORD! ***"
             break
+        } else {
+            giveHint($userInput)
         }
     } else {
         Write-Output "Alleen getallen tussen de 1 en de $($maxValue)!"
+        userGuess
+    }
+}
+
+function giveHint {
+    if ($userInput -lt $answer) {
+        Write-Output "Het getal wat je zoekt is groter!"
+        userGuess
+    }
+    if ($userInput -gt $answer) {
+        Write-Output "Het getal wat je zoekt is kleiner!"
         userGuess
     }
 }
